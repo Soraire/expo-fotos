534c0172fc66b2d724ba8181c30c7402a2e6ec58
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-06-26 10:59:27 -0300

    ahora anda el expo

--- App.js
+++ App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen.js';
-import PhotoScreen from './src/screens/PhotoScreen.js';
+//import PhotoScreen from './src/screens/PhotoScreen.js';
 
 const AppNavigator = createStackNavigator(
   {
     Home: { screen: HomeScreen },
-    Photo: { screen: PhotoScreen },
+  //  Photo: { screen: PhotoScreen },
   },
   {
     initialRouteName: 'Home',
